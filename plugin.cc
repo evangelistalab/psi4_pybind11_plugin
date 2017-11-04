a342d94f0b49270499aa0a60ff3f4bf8413347f9
Francesco Evangelista <francesco.evangelista@gmail.com>
2017-11-04 09:50:37 -0400

    Cleanup.

--- plugin.cc
+++ plugin.cc
@@ -21,51 +21,54 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License along
+ * You should have received a copy of the GNU Lesser General Public License
+ * along
  * with Psi4; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
  * @END LICENSE
  */
 
-#include "psi4/psi4-dec.h"
-#include "psi4/libparallel/parallel.h"
-#include "psi4/liboptions/liboptions.h"
-#include "psi4/libpsio/psio.hpp"
-#include "psi4/libmints/wavefunction.h"
 #include "psi4/libmints/basisset.h"
-
+#include "psi4/libmints/wavefunction.h"
+#include "psi4/liboptions/liboptions.h"
+#include "psi4/libpsi4util/PsiOutStream.h"
+#include "psi4/libpsio/psio.hpp"
+#include "psi4/psi4-dec.h"
+#include "psi4/psi4-dec.h"
 
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
-namespace psi{ namespace pybind_plugin {
+namespace psi {
+namespace pybind_plugin {
 
-int read_options(std::string name, Options& options)
-{
-    if (name == "PYBIND_PLUGIN"|| options.read_globals()) {
-        /*- The amount of information printed to the output file -*/
-        options.add_int("PBP_C_PRINT", 1);
-        options.add_double("PBP_C_CONV", 1e-6);
-        options.add_str("PBP_C_TYPE", "CONV", "DF CONV");
-    }
-    return true;
+int read_options(std::string name, Options &options) {
+  if (name == "PYBIND_PLUGIN" || options.read_globals()) {
+    /*- The amount of information printed to the output file -*/
+    options.add_int("PBP_C_PRINT", 1);
+    options.add_double("PBP_C_CONV", 1e-6);
+    options.add_str("PBP_C_TYPE", "CONV", "DF CONV");
+  }
+  return true;
 }
 
-SharedWavefunction myplugin(SharedWavefunction ref_wfn, Options& options)
-{
-    options.set_current_module("PYBIND_PLUGIN");
-    outfile->Printf("\n Number of basis functions: %d",ref_wfn->basisset()->nbf());
-    outfile->Printf("\n Ref:   %s",options.get_str("REFERENCE").c_str());
-    outfile->Printf("\n Print: %d",options.get_int("PBP_C_PRINT"));
-    outfile->Printf("\n Conv:  %f",options.get_double("PBP_C_CONV"));
-    outfile->Printf("\n Type:  %s",options.get_str("PBP_C_TYPE").c_str());
-    printf("\n Ref:   %s",options.get_str("REFERENCE").c_str());
-    printf("\n Print: %d",options.get_int("PBP_C_PRINT"));
-    printf("\n Conv:  %f",options.get_double("PBP_C_CONV"));
-    printf("\n Type:  %s",options.get_str("PBP_C_TYPE").c_str());
+SharedWavefunction myplugin(SharedWavefunction ref_wfn, Options &options) {
+  options.set_current_module("PYBIND_PLUGIN");
+  outfile->Printf("\n Number of basis functions: %d",
+                  ref_wfn->basisset()->nbf());
+  outfile->Printf("\n Ref:   %s", options.get_str("REFERENCE").c_str());
+  outfile->Printf("\n Print: %d", options.get_int("PBP_C_PRINT"));
+  outfile->Printf("\n Conv:  %f", options.get_double("PBP_C_CONV"));
+  outfile->Printf("\n Type:  %s", options.get_str("PBP_C_TYPE").c_str());
 
-    return ref_wfn;
+  printf("\n Ref:   %s", options.get_str("REFERENCE").c_str());
+  printf("\n Print: %d", options.get_int("PBP_C_PRINT"));
+  printf("\n Conv:  %f", options.get_double("PBP_C_CONV"));
+  printf("\n Type:  %s", options.get_str("PBP_C_TYPE").c_str());
+  printf("\n");
+
+  return ref_wfn;
 }
 
 PYBIND11_PLUGIN(pybind_plugin) {
@@ -76,6 +79,5 @@ PYBIND11_PLUGIN(pybind_plugin) {
 
   return m.ptr();
 }
-
-}} // End namespaces
-
+}
+} // End namespaces
